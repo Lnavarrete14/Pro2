48af8e0d74c45bf5e69b1e0166292cbf7ccee6f7
Luis Navarrete <Luis Navarrete@DESKTOP-9DB9RFM>
2017-01-24 23:33:10 -0500

    Agregar archivos de proyecto.

---
+++ Pro1/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Pro1.Startup))]
+namespace Pro1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
